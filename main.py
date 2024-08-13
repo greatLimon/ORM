f910ff79e5e34073e3c796c6144193fc4107effe
Ivan <gordeevid@yandex.ru>
2024-08-13 15:00:02 +0300

    add models

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import sqlalchemy
+import psycopg2
+from models import create_tables
+
+def start_db():
+    # basetype = 'postgresql'
+    # login = 'postgres'
+    # password = '123321'
+    # port = 'localhost:5432'
+    # base = 'netologyORM_db'
+    # DSN = f'{basetype}://{login}/{password}@{port}/{base}'
+    DSN = "postgresql://postgres/123321@localhost:5432/netologyORM_db"
+    engine = sqlalchemy.create_engine(DSN)
+    create_tables(engine)
+
 def main():
-    ...
-    
+    # try:
+    #     start_db()
+    # except:
+    #     print('Error!')
+    start_db()
+
 if __name__ == '__main__':
     main()
---
+++ models.py
@@ -0,0 +1,47 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+
+Base = declarative_base()
+
+# Tables
+class Publisher(Base):
+    __tablename__ = 'publisher'
+    id = sq.Column(sq.Integer, primary_key = True)
+    name = sq.Column(sq.String(length=60), unique=True)
+
+class Shop(Base):
+    __tablename__ = 'shop'
+    id = sq.Column(sq.Integer, primary_key = True)
+    name = sq.Column(sq.String(length=60), unique=True)
+
+class Book(Base):
+    __tablename__ = 'book'
+    id = sq.Column(sq.Integer, primary_key = True)
+    title = sq.Column(sq.String(length=60), unique=60)
+    id_publisher = sq.Column(sq.ForeignKey('publisher.id'), nullable=False)
+    publisher = relationship(Publisher, backref="Book")
+
+class Stock(Base):
+    __tablename__='stock'
+    id = sq.Column(sq.Integer, primary_key = True)
+    id_book = sq.Column(sq.ForeignKey('book.id'), nullable= False)
+    id_shop = sq.Column(sq.ForeignKey('shop.id'), nullable= False)
+    count = sq.Column(sq.Integer, nullable=False)
+    book = relationship(Book, backref = 'book')
+    shop = relationship(Shop, backref = 'shop')
+
+class Sale(Base):
+    __tablename__ = 'sale'
+    id = sq.Column(sq.Integer, primary_key = True)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale = sq.Column(sq.Date, nullable=False)
+    id_stock = sq.Column(sq.ForeignKey('stock.id'), nullable=False)
+    count = sq.Column(sq.Integer, nullable= False)
+# -Tables
+
+def create_tables(engine:sq.engine)->sessionmaker:
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind = engine)
+    session = Session()
+    print(type(session))
+    return session
